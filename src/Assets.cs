38dc2e1da1d01efae4a7e4795738e4ab7d725175
schwer <42115696+itsschwer@users.noreply.github.com>
2026-06-09 21:36:27 +1000

    Try improve asset loading

--- src/Assets.cs
+++ src/Assets.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using UnityEngine;
 
 namespace PaleOilSoap
@@ -7,28 +8,41 @@ namespace PaleOilSoap
     {
         public bool initialized => (disableSound && overrideSound && constrainedSound);
 
-        public readonly AudioClip disableSound;
-        public readonly AudioClip overrideSound;
-        public readonly AudioClip constrainedSound;
+        public AudioClip disableSound { get; private set; }
+        public AudioClip overrideSound { get; private set; }
+        public AudioClip constrainedSound { get; private set; }
 
         public Assets()
         {
-            string path = Path.Combine(Application.streamingAssetsPath, "aa", "StandaloneWindows64", "sfxstatic_assets_shared.bundle");
+            // Assuming existing loaded bundle will not be unloaded (given "sfxstatic_assets_shared")
+            string guid = "81cdd0803a8bcfb81097fa4b8f33bb6e.bundle";
+            AssetBundle assetBundle = AssetBundle.GetAllLoadedAssetBundles().FirstOrDefault((bundle) => bundle.name == guid);
+            if (assetBundle != null) {
+                Plugin.Logger.LogDebug("Found target asset bundle in loaded asset bundles.");
+                LoadSounds(assetBundle);
+            }
+            else {
+                Plugin.Logger.LogDebug("Could not find target asset bundle in loaded asset bundles, attempting to load manually.");
+                string path = Path.Combine(Application.streamingAssetsPath, "aa", "StandaloneWindows64", "sfxstatic_assets_shared.bundle");
+                assetBundle = AssetBundle.LoadFromFile(path);
+                LoadSounds(assetBundle);
+                assetBundle.Unload(false);
+            }
 
-            AssetBundle assetBundle = AssetBundle.LoadFromFile(path);
+            if (initialized) Plugin.Logger.LogDebug("Located assets.");
+            else Plugin.Logger.LogWarning("Failed to locate assets.");
+        }
 
+        private void LoadSounds(AssetBundle assetBundle)
+        {
             if (assetBundle == null) {
-                Plugin.Logger.LogWarning("Failed to locate asset bundle.");
+                Plugin.Logger.LogWarning("Failed to locate target asset bundle.");
                 return;
             }
 
             disableSound = assetBundle.LoadAsset<AudioClip>("Assets/Audio/SFX/Heroes/Hornet/Crest Weapons/hornet_hunter_needleart_slash_2.wav");
             overrideSound = assetBundle.LoadAsset<AudioClip>("Assets/Audio/SFX/Enemy/Bosses/Hornet/hornet_needle_catch.wav");
             constrainedSound = assetBundle.LoadAsset<AudioClip>("Assets/Audio/SFX/sword_hit_reject.wav");
-            assetBundle.Unload(false);
-
-            if (initialized) Plugin.Logger.LogDebug("Located assets.");
-            else Plugin.Logger.LogWarning("Failed to locate assets.");
         }
     }
 }
